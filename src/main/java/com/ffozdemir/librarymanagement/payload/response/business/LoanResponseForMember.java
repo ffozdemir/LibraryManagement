de345f4c6e22508943e370c126c75587752f3b6f
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-03-13 14:16:54 +0100

    - remove NoArgsConstructor from LoanResponse classes, Abstract already has it

--- src/main/java/com/ffozdemir/librarymanagement/payload/response/business/LoanResponseForMember.java
+++ src/main/java/com/ffozdemir/librarymanagement/payload/response/business/LoanResponseForMember.java
@@ -4,13 +4,11 @@ import com.ffozdemir.librarymanagement.payload.response.abstracts.BaseLoanRespon
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @SuperBuilder
 public class LoanResponseForMember extends BaseLoanResponse {
 }
